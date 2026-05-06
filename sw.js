158e390532acefc8c5210174077855b5236e7223
Dimitri <zufferey@gmail.com>
2026-05-06 10:38:04 +0200

    Add files via upload
    v280

--- sw.js
+++ sw.js
@@ -1602,7 +1602,7 @@
 //       Stale-while-revalidate sur CSS/JS, channel postMessage 'GET_VERSION'.
 // v39..v21 : voir docs/CHANGELOG.md.
 
-const CACHE_VERSION = 'cas-in-v114';
+const CACHE_VERSION = 'cas-in-v116';
 
 // ─── Ressources critiques (HTML/JSON/CSS/JS) ───
 // Liste maintenue à la main car peu volatile. Les FICHES sont lues
@@ -1718,6 +1718,7 @@ const STATIC_ASSETS = [
   './js/components/fiche-common.js',
   './js/components/fiche-reader.js',
   './js/components/scene-npcs.js',
+  './js/components/scene-banners-carousel.js',
   './data/search-index.json',
   './data/npcs.json',
   './data/npc-arcs.json',
